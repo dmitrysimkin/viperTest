58544e62f30e7a6ff298b3a811dbf57145ea5a6c
Dmitry Simkin <d.simkin@godeltech.com>
2020-10-05 22:58:48 +0300

    Initial Commit

---
+++ ViperTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViperTest
+//
+//  Created by Dzmitry Simkin on 10/5/20.
+//  Copyright © 2020 Dzmitry Simkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
